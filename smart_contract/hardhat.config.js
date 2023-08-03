8d5956d15c1713823d061b6829f412ff36f5f82a
Dexter49 <dexterousguru49@yahoo.com>
2023-08-03 21:29:31 +0100

    another slight tweak - changed Solidity from 0.8.0 to 0.8.19

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,8 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-sepolia.g.alchemy.com/v2/pavQx7G0GV7DmmjGqvk3P4rfYl-FnD0p
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
   solidity: "0.8.19",
 };
